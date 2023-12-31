86a1791537a4a74cf964aaff4a8087e4e0b7ac24
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-31 17:48:11 +0100

    resolved

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+    public static void main(String[] args) {
+
+    }
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 0;
+        while(counter <= printToInclusive){
+            if(counter <= 1){
+
+            } else if (counter  == 2 || counter == 3){
+                System.out.println(counter);
+            }else if (counter % 2 == 0 || counter % 3 == 0 || counter % 5 == 0){
+
+            }else{
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
+
