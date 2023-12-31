82d6785987e19d12e12f1db272f6688c667da85d
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-31 16:07:04 +0100

    resolved

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public static void main(String[] args) {
-
     }
     public void printPower(int power) {
         int counter = 0;
